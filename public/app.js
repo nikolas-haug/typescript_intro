fd610ab353fb64226460d5c73cc7fb69b21b4359
Nikolas Haug <nikohaug21@gmail.com>
2021-06-11 15:33:08 -0500

    add classes

--- public/app.js
+++ public/app.js
@@ -1,9 +1,28 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
+// classes
+var Invoice = /** @class */ (function () {
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(function (inv) {
+    console.log(inv.client, inv.amount, inv.format());
+});
 var form = document.querySelector('.new-item-form');
-// console.log(form.children);
 // inputs
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,33 @@
-// const anchor = document.querySelector('a')!;
+// classes
+class Invoice {
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
 
-// console.log(anchor.href);
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ) {}
 
-// const form = document.querySelector('form')!;
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
