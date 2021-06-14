647f2f193c9cef6c0f337e9b1880f6a860dab3cc
Nikolas Haug <nikohaug21@gmail.com>
2021-06-14 13:38:10 -0500

    render html templates

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@
 // import { Payment } from './classes/Payment.js';
 // import { HasFormatter } from './interfaces/HasFormatter.js';
 import { Invoice } from "./classes/invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 // let docOne: HasFormatter;
 // let docTwo: HasFormatter;
@@ -25,6 +26,9 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -34,5 +38,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@
 // import { HasFormatter } from './interfaces/HasFormatter.js';
 
 import { Invoice } from "./classes/invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
@@ -38,6 +39,10 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
@@ -48,5 +53,5 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
 
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
